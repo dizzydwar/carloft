87dc06dae98b8f967c62e9f7584347f044d3d138
hannes dolde <hannes.dolde@gmx.de>
2016-07-12 02:10:46 +0200

    font and slides

--- wp-content/themes/carloft_custom/index.php
+++ wp-content/themes/carloft_custom/index.php
@@ -33,7 +33,19 @@ get_header(); ?>
                             </div>
                             
                             <div class="slide">                     
-                                   <img src="<?php echo  get_stylesheet_directory_uri(); ?>/images/malte_jaeger_carloft-3.jpg" alt=""/>
+                                   <img src="<?php echo  get_stylesheet_directory_uri(); ?>/images/malte_jaeger_carloft-3_edit.jpg" alt=""/>
+                                   <div class="overlay col-sm-12 col-md-6">
+                                          <div class="headline-group">
+                                                 <h3>CarLoggia</h3>
+                                                 <h2>Erdgeschoss im 4. Stock</h2>
+                                                 <p class="copy">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </p>
+                                          </div>
+
+                                   </div>
+                            </div>
+                            
+                            <div class="slide">                     
+                                   <img src="<?php echo  get_stylesheet_directory_uri(); ?>/images/malte_jaeger_carloft_edit_2.jpg" alt=""/>
                                    <div class="overlay col-sm-12 col-md-6">
                                           <div class="headline-group">
                                                  <h3>CarLoggia</h3>
